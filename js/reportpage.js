a7988507388c2cabfdfe09a1c22e218407572d2a
SiSiQin022 <s3365736@student.rmit.edu.au>
2017-04-28 16:27:13 +1000

    Initial commit

---
+++ js/reportpage.js
@@ -0,0 +1,21 @@
+function actionFormatter(value, row, index) {
+    return [
+        '<a class="remove ml10" href="javascript:void(0)" title="Remove">',
+        '<i class="glyphicon glyphicon-remove"></i>',	
+        '</a>',
+		  '<a class="edit ml10" href="javascript:void(0)" title="Edit">',
+        '<i class="	glyphicon glyphicon-download-alt"></i>',
+        '</a>'
+    ].join('');
+}
+
+window.actionEvents = {
+    'click .edit': function (e, value, row, index) {
+        alert('You click edit icon, row: ' + JSON.stringify(row));
+        console.log(value, row, index);
+    },
+    'click .remove': function (e, value, row, index) {
+        alert('You click remove icon, row: ' + JSON.stringify(row));
+        console.log(value, row, index);
+    }
+};
